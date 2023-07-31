7e32bc180aede0ff7f0339b8c236266999f1f6e4
Carlos <cgaleano@prolesys.com>
2023-07-31 14:55:30 -0400

    tomando numero telefonico de variables de entorno

--- src/handler/ws.handler.ts
+++ src/handler/ws.handler.ts
@@ -6,6 +6,6 @@ const wsTransporter =  container.get('ws.tranporter');
 export const job = new CronJob('*/10 * * * * *', async () => {
 	const d = new Date();
 	console.log('Send message every tenth seconds:', d);
-    const result = await wsTransporter.sendMsg({message: '✌️✌️✌️✌️', phone: ''})
+    const result = await wsTransporter.sendMsg({message: 'Mensaje automatizado - TEST', phone: process.env['PHONE_NUMBER']})
     console.log(result)
 });
