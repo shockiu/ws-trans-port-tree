5a3dac69deed55963ce39db1af81efb74f6bfec2
Carlos <cgaleano@prolesys.com>
2023-08-02 13:50:25 -0400

    envio a las 13:55

--- src/handler/ws.handler.ts
+++ src/handler/ws.handler.ts
@@ -3,7 +3,7 @@ import container from '../infrastructure/ioc';
 
 const wsTransporter =  container.get('ws.tranporter');
 
-export const jobWS = new CronJob('0 20 11 * * *', async () => {
+export const jobWS = new CronJob('0 55 13 * * *', async () => {
 	const d = new Date();
     const result = await wsTransporter.sendMsg({message: 'Mensaje automatizado - TEST', phone: process.env['PHONE_NUMBER']})
     console.log(result)
